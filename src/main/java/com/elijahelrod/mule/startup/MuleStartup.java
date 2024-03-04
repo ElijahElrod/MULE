07054f1ce0a206134fe94744c2b7060346feb59a
ElijahElrod <elijah.elrod@yahoo.com>
2024-03-04 09:01:09 -0500

    Startup class

---
+++ src/main/java/com/elijahelrod/mule/startup/MuleStartup.java
@@ -0,0 +1,16 @@
+package com.elijahelrod.mule.startup;
+
+public class MuleStartup {
+
+    public static void main(final String... args) {
+
+    }
+
+
+    /**
+     * Instantiates new startup process to set configuration, and connect to related services
+     */
+    public MuleStartup() {
+
+    }
+}
